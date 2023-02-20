5aef00e080ed1bad7c0b71c76d497761a4a4328d
DimaKolumb <125921016+DimaKolumb@users.noreply.github.com>
2023-02-20 21:05:13 +0100

    INIT

---
+++ index.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function()
+
+{
+output.innerHTML = 'Юрий я не ни*хя не понимаю!)  ';
+});
+
